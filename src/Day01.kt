49ee89c9b488e57b6a97fc47ab704e9815d519e9
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-12-01 13:40:39 -0500

    Day01 Part1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        var result = 0
+
+        for ((index, depth) in depths.withIndex()) {
+            if (index == depths.size - 1) break
+            if (depths[index + 1] > depth) {
+                result++
+            }
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))

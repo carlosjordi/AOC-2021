49c812caca4f00a2db3681c2b03aef6f3512dfaa
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-12-02 17:21:26 -0500

    Day01 part2 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val depths = input.map { it.toInt() }
-        var result = 0
-
-        for ((index, depth) in depths.withIndex()) {
-            if (index == depths.size - 1) break
-            if (depths[index + 1] > depth) {
-                result++
-            }
-        }
-        return result
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    val depths = input.map { it.toInt() }
+    var result = 0
+
+    for ((index, depth) in depths.withIndex()) {
+        if (index == depths.size - 1) break
+        if (depths[index + 1] > depth) {
+            result++
+        }
+    }
+    return result
+}
+
+fun part2(input: List<String>): Int {
+    val depths = input.map(String::toInt)
+    var result = 0
+
+    for ((index, depth) in depths.withIndex()) {
+        if (index == depths.size - 3) break
+        if (depths[index + 3] + depths[index + 2] + depths[index + 1] >
+            depths[index + 2] + depths[index + 1] + depth
+        ) {
+            result++
+        }
+    }
+    return result
 }

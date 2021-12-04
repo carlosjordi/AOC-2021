3119b09c50865fccae2b0bca0879bbe34a5e8d82
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-12-03 22:36:27 -0500

    Day03 Part2 done

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,5 +1,3 @@
-import jdk.jfr.Threshold
-
 fun main() {
     fun part1(input: List<String>): Int {
         val diagnostic = BinaryDiagnostic()
@@ -15,7 +13,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var reducedOxygen = input
+        // oxygen
+        for (i in 0..11) {
+            reducedOxygen = reducedOxygen.reduceList(i)
+            if (reducedOxygen.size == 1) break
+        }
+        val oxygen = reducedOxygen[0].toInt(2)
+
+        var reducedCo2 = input
+        // co2
+        for (i in 0..11) {
+            reducedCo2 = reducedCo2.reduceList(i, false)
+            if (reducedCo2.size == 1) break
+        }
+        val co2 = reducedCo2[0].toInt(2)
+
+        return oxygen * co2
     }
 
     val input = readInput("Day03")
@@ -34,5 +48,31 @@ data class BinaryDiagnostic(val output: MutableList<Int> = mutableListOf(0, 0, 0
         return output.map { amount ->
             if (amount > threshold) 0 else 1
         }.joinToString(separator = "").toInt(2)
+    }
+}
+
+private fun List<String>.reduceList(index: Int, isOxygenRating: Boolean = true): List<String> {
+    if (isOxygenRating) {
+        val ones = count {
+            it[index] == '1'
+        }
+        return filter {
+            if (ones >= size / 2.0) {
+                it[index] == '1'
+            } else {
+                it[index] == '0'
+            }
+        }
+    } else {
+        val zeros = count {
+            it[index] == '0'
+        }
+        return filter {
+            if (zeros <= size / 2.0) {
+                it[index] == '0'
+            } else {
+                it[index] == '1'
+            }
+        }
     }
 }
